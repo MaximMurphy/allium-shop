ac5b3d1a921583af939a452917aacd8216975852
Maxim Murphy <maxim.murphy8@gmail.com>
2025-04-01 11:25:38 -0400

    Adjusted vegetables images for mobile

--- app/components/LandingVegetables.tsx
+++ app/components/LandingVegetables.tsx
@@ -8,36 +8,43 @@ import SweetOnion from '../assets/veg/sweet-onion.png';
 export function LandingVegetables() {
   return (
     <div className="w-full h-full flex items-start justify-start">
-      <div className="w-full flex items-end justify-start">
+      <div className="hidden md:flex w-full items-end justify-start">
         <img
           src={Shallot}
           alt="Shallot"
-          className="w-40 h-40 object-contain relative z-10 -rotate-25 -ml-8"
+          className="w-24 md:w-40 h-24 md:h-40 object-contain relative z-10 -rotate-35 -ml-8 md:-ml-12"
         />
         <img
           src={Leek}
           alt="Leek"
-          className="w-64 h-64 object-contain relative -ml-32"
+          className="w-48 md:w-64 h-48 md:h-64 object-contain relative -ml-24 md:-ml-32"
         />
         <img
           src={SweetOnion}
           alt="Sweet Onion"
-          className="w-40 h-40 object-contain relative -ml-28 z-10"
+          className="w-24 md:w-40 h-24 md:h-40 object-contain relative -ml-24 md:-ml-28 z-10"
         />
         <img
           src={Garlic}
           alt="Garlic"
-          className="w-32 h-32 object-contain relative -ml-4 z-20"
+          className="w-20 md:w-32 h-20 md:h-32 object-contain relative -ml-2 md:-ml-4 z-20"
         />
         <img
           src={RedOnion}
           alt="Red Onion"
-          className="w-40 h-40 object-contain relative -ml-2"
+          className="w-24 md:w-40 h-24 md:h-40 object-contain relative md:-ml-2"
         />
         <img
           src={GreenOnion}
           alt="Green Onion"
-          className="w-48 h-48 object-contain relative -ml-24 z-10"
+          className="w-28 md:w-48 h-28 md:h-48 object-contain relative -ml-12 md:-ml-24 z-10"
+        />
+      </div>
+      <div className="flex md:hidden w-full items-center justify-center">
+        <img
+          src={Garlic}
+          alt="Garlic"
+          className="w-48 h-48 object-contain relative z-10"
         />
       </div>
     </div>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -19,12 +19,12 @@ export default function Homepage() {
         <div className="w-full">
           <FullWidthImage src={alliumText} alt="Allium" />
         </div>
-        <section className="h-full w-full flex flex-col justify-between">
-          <div className="w-full flex flex-col lg:flex-row gap-8 max-w-7xl mx-auto px-8">
+        <section className="h-full w-full flex flex-col justify-between pt-4 pb-12 md:pb-0 md:pt-0">
+          <div className="w-full flex flex-col lg:flex-row gap-4 md:gap-8 max-w-7xl mx-auto px-8">
             <div className="w-full lg:w-1/2 pt-4">
               <LandingVegetables />
             </div>
-            <div className="w-full lg:w-1/2 flex pt-20">
+            <div className="w-full lg:w-1/2 flex md:pt-20">
               <p className="h-full text-3xl md:text-5xl text-allium-green italic text-center lg:text-right">
                 Browse our collection of culinary inspired designs. Perfect for
                 any chef, home cook, or food lover.
