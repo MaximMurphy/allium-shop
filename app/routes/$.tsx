016c5035703d896a6e76a8e82a66414e66a8a2fd
Maxim Murphy <maxim.murphy8@gmail.com>
2025-04-07 15:25:10 -0400

    Added Catch All error page

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,11 +1,18 @@
-import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import {Link} from '@remix-run/react';
 
-export async function loader({request}: LoaderFunctionArgs) {
-  throw new Response(`${new URL(request.url).pathname} not found`, {
-    status: 404,
-  });
-}
-
-export default function CatchAllPage() {
-  return null;
+export default function CatchAll() {
+  return (
+    <div className="w-full min-h-[100svh] md:h-full flex flex-col items-center justify-start gap-12 pt-12 md:pt-32 text-allium-dark-brown">
+      <h1 className="text-5xl font-medium">404 - Page Not Found</h1>
+      <p className="text-3xl">
+        Oops! The page you are looking for does not exist.
+      </p>
+      <Link
+        to="/"
+        className="w-fit bg-allium-dark-green text-allium-cream px-2 md:px-8 py-2 md:py-4 rounded-lg border-2 border-allium-dark-green hover:bg-allium-light-green transition duration-300 text-center"
+      >
+        Return Home
+      </Link>
+    </div>
+  );
 }
