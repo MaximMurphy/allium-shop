5a629e10baedc5943d0a94427a882f532604d7e9
Maxim Murphy <maxim.murphy8@gmail.com>
2025-04-10 20:52:58 -0400

    removed scrollbar flash from screen

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 export function Container({children, className = ''}: ContainerProps) {
   return (
     <div
-      className={`w-full max-w-7xl mx-auto px-4 md:px-8 overflow-x-hidden ${className}`}
+      className={`w-full max-w-7xl mx-auto px-4 md:px-8 overflow-x-hidden scrollbar-hidden ${className}`}
     >
       {children}
     </div>
